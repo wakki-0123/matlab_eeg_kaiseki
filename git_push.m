0175ecf12aabff476f8e98f2d5bdf4ea4b68db79
wakki-0123 <s2131163ug@s.chibakoudai.jp>
2024-01-25 12:09:20 +0900

    commit_0125

--- git_push.m
+++ git_push.m
@@ -5,8 +5,6 @@ function git_push()
 !git add *
 !git commit -m commit_0125 
 % 0125は日付にしてある　逐次変更すること
-!git remote add origin https://github.com/wakki-0123/matlab_eeg_kaiseki.git
-!git branch eeglab
-% branch 名も逐次変更すること
+
 
 !git push origin eeglab
